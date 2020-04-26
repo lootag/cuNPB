dc44443cf839bcc9ef7efa9e0921cac9c0d46aff
Alex <luigi.taglialatela1996@gmail.com>
2020-04-26 20:48:49 +0200

    testing double input GetKernel

--- main.cu
+++ main.cu
@@ -8,8 +8,8 @@ int main()
     char *directory = "/content/src"; 
     int ret;
     ret = chdir(directory);
-    Eigen::MatrixXf Left = from_csv("train.csv", 4, 3);
-    Eigen::MatrixXf Right = from_csv("test.csv", 3, 2);
+    //Eigen::MatrixXf Left = from_csv("train.csv", 4, 3);
+    //Eigen::MatrixXf Right = from_csv("test.csv", 3, 2);
     
     /*
     Eigen::MatrixXf Result = Multiply(Left, Right.transpose());
@@ -21,7 +21,28 @@ int main()
         }
     }
     */
-    Eigen::MatrixXf Result = GetKernel(Left);
+    Eigen::MatrixXf A(3,4);
+    Eigen::MatrixXf B(3,5);
+    float tmp = 1;
+    for(int row = 0; row != A.rows(); row++)
+    {
+        for(int col = 0; col != A.cols(); col++)
+        {
+            A(row, col) = tmp;
+            tmp += 1;
+        }
+    }
+    tmp = 1;
+    for(int row = 0; row != B.rows(); row++)
+    {
+        for(int col = 0; col != B.cols(); col++)
+        {
+            B(row, col) = tmp;
+            tmp += 1;
+        }
+    }
+
+    Eigen::MatrixXf Result = GetKernel(A, B);
     for(int row = 0; row != Result.rows(); row++)
     {
         for(int col = 0; col != Result.cols(); col++)
