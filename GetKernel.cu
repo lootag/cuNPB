8b6fce0b1861f17cf4412e07be0f40818a7dfd49
Alex <luigi.taglialatela1996@gmail.com>
2020-04-30 15:07:42 +0200

    finalizing kernel computation

--- GetKernel.cu
+++ GetKernel.cu
@@ -10,11 +10,12 @@
 
 using std::vector;
 
-__global__ void getKernel(const float *a, const float *b, float *c, int rows, int cols, int rowsB) 
+__global__ void getKernel(const float *a, const float *b, float *c, int rows, int cols, int rowsB, float sigma_2, float l) 
 {
   // Compute each thread's global row and column index
     int row = blockIdx.y * blockDim.y + threadIdx.y;
     int col = blockIdx.x * blockDim.x + threadIdx.x;
+    float l_2 = pow(l, 2);
 
   // Iterate over row, and down column
     if(row < rows && col < rowsB)
@@ -24,6 +25,7 @@ __global__ void getKernel(const float *a, const float *b, float *c, int rows, in
         {
             c[col + row * rowsB] += (a[k + row * cols] - b[col + k * rowsB]) * (a[k + row * cols] - b[col + k * rowsB]);
         }
+        c[col + row + rowsB] = sigma_2*exp(-0.5*sqrt(c[col + row + rowsB])/l_2)s
     }
   
   
@@ -32,93 +34,97 @@ __global__ void getKernel(const float *a, const float *b, float *c, int rows, in
 
 
 Eigen::MatrixXf GetKernel(Eigen::MatrixXf A, Eigen::MatrixXf B) 
-{
-  // Matrix size of 4 x 3;
-  int rows = A.rows();
-  int cols = A.cols();
-  int rowsB = B.rows();
-  Eigen::MatrixXf C(rows,rowsB);
-  
-
-  
-
-  // Size (in bytes) of matrix
-  size_t bytes_a = rows * cols * sizeof(float);
-  size_t bytes_b = cols * rowsB * sizeof(float);
-  size_t bytes_c = rows * rowsB * sizeof(float);
-
-  // Host vectors
-  vector<float> h_a(rows * cols);
-  vector<float> h_b(cols * rowsB);
-  vector<float> h_c(rows * rowsB);
-
-  for(int row = 0; row != A.rows(); row++)
-  {
-      for(int col = 0; col != A.cols(); col++)
-      {
-          h_a[col + row*cols] = A(row,col);
-      }
-  }
-
-  for(int row = 0; row != B.transpose().rows(); row++)
-  {
-      for(int col = 0; col != B.transpose().cols(); col++)
-      {
-          h_b[col + row * B.transpose().cols()] = B.transpose()(row,col);
-      }
-  }
-
- 
-
-
-  
-
-
-  // Allocate device memory
-  float *d_a, *d_b, *d_c;
-  cudaMalloc(&d_a, bytes_a);
-  cudaMalloc(&d_b, bytes_b);
-  cudaMalloc(&d_c, bytes_c);
-
-  // Copy data to the device
-  cudaMemcpy(d_a, h_a.data(), bytes_a, cudaMemcpyHostToDevice);
-  cudaMemcpy(d_b, h_b.data(), bytes_b, cudaMemcpyHostToDevice);
-
-  // Threads per CTA dimension
-  int THREADS = 32;
-
-  // Blocks per grid dimension (assumes THREADS divides N evenly)
-  float fTHREADS = THREADS;
-  float frows = rows;
-  float fBLOCKS = ceil(frows/fTHREADS);
-  int BLOCKS = fBLOCKS;
-  
-
-  // Use dim3 structs for block  and grid dimensions
-  dim3 threads(THREADS, THREADS);
-  dim3 blocks(BLOCKS, BLOCKS);
-
-  // Launch kernel
-  getKernel<<<blocks, threads>>>(d_a, d_b, d_c, rows, cols, rowsB);
-
-  // Copy back to the host
-  cudaMemcpy(h_c.data(), d_c, bytes_c, cudaMemcpyDeviceToHost);
-
-  for(int row = 0; row != C.rows(); row++)
-  {
-      for(int col = 0; col != C.cols(); col++)
-      {
-          C(row,col) = h_c[col + row*C.cols()];
-      }
-  }
+{   
+    if(A.cols() != B.cols())
+    {
+        std::cout << "The matrices you've given me have different numbers of columns. Now I'll die :(" << std::endl;
+    }
+    // Matrix size of 4 x 3;
+    int rows = A.rows();
+    int cols = A.cols();
+    int rowsB = B.rows();
+    Eigen::MatrixXf C(rows,rowsB);
 
 
 
 
-  // Free memory on device
-  cudaFree(d_a);
-  cudaFree(d_b);
-  cudaFree(d_c);
+    // Size (in bytes) of matrix
+    size_t bytes_a = rows * cols * sizeof(float);
+    size_t bytes_b = cols * rowsB * sizeof(float);
+    size_t bytes_c = rows * rowsB * sizeof(float);
 
-  return C;
+    // Host vectors
+    vector<float> h_a(rows * cols);
+    vector<float> h_b(cols * rowsB);
+    vector<float> h_c(rows * rowsB);
+
+    for(int row = 0; row != A.rows(); row++)
+    {
+        for(int col = 0; col != A.cols(); col++)
+        {
+            h_a[col + row*cols] = A(row,col);
+        }
+    }
+
+    for(int row = 0; row != B.transpose().rows(); row++)
+    {
+        for(int col = 0; col != B.transpose().cols(); col++)
+        {
+            h_b[col + row * B.transpose().cols()] = B.transpose()(row,col);
+        }
+    }
+
+
+
+
+
+
+
+    // Allocate device memory
+    float *d_a, *d_b, *d_c;
+    cudaMalloc(&d_a, bytes_a);
+    cudaMalloc(&d_b, bytes_b);
+    cudaMalloc(&d_c, bytes_c);
+
+    // Copy data to the device
+    cudaMemcpy(d_a, h_a.data(), bytes_a, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_b, h_b.data(), bytes_b, cudaMemcpyHostToDevice);
+
+    // Threads per CTA dimension
+    int THREADS = 32;
+
+    // Blocks per grid dimension (assumes THREADS divides N evenly)
+    float fTHREADS = THREADS;
+    float frows = rows;
+    float fBLOCKS = ceil(frows/fTHREADS);
+    int BLOCKS = fBLOCKS;
+
+
+    // Use dim3 structs for block  and grid dimensions
+    dim3 threads(THREADS, THREADS);
+    dim3 blocks(BLOCKS, BLOCKS);
+
+    // Launch kernel
+    getKernel<<<blocks, threads>>>(d_a, d_b, d_c, rows, cols, rowsB);
+
+    // Copy back to the host
+    cudaMemcpy(h_c.data(), d_c, bytes_c, cudaMemcpyDeviceToHost);
+
+    for(int row = 0; row != C.rows(); row++)
+    {
+        for(int col = 0; col != C.cols(); col++)
+        {
+            C(row,col) = h_c[col + row*C.cols()];
+        }
+    }
+
+
+
+
+    // Free memory on device
+    cudaFree(d_a);
+    cudaFree(d_b);
+    cudaFree(d_c);
+
+    return C;
 }
