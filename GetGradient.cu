ae308b508220138bd614e672efb42ccbfd01a753
Alex <luigi.taglialatela1996@gmail.com>
2020-05-02 01:13:57 +0200

    checking dl 7

--- GetGradient.cu
+++ GetGradient.cu
@@ -28,8 +28,8 @@ Eigen::MatrixXf GetGradient(Eigen::MatrixXf Train, Eigen::MatrixXf labels, float
     dl = Multiply(dl, K_inv.transpose());
     dl = Multiply(dl, labels.transpose());
     Eigen::MatrixXf trace_dl(1,1);
-    /*
     trace_dl(0,0) = 0.5*Multiply(K_inv, dK_dl.transpose()).trace();
+    /*
     dl = dl - trace_dl;
     gradient(0,0) = dsigma(0,0); 
     gradient(0,1) = dl(0,0);
