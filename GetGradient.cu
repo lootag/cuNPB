647be0bd39b94af87ad658315f71fbc40b79083e
Alex <luigi.taglialatela1996@gmail.com>
2020-05-02 01:23:08 +0200

    checking dsigma 2

--- GetGradient.cu
+++ GetGradient.cu
@@ -14,7 +14,7 @@ Eigen::MatrixXf GetGradient(Eigen::MatrixXf Train, Eigen::MatrixXf labels, float
     Eigen::MatrixXf dK_dsigma = GetKernel(Train, Train, sigma, l, type_dK_dsigma);   
     Eigen::MatrixXf dK_dl = GetKernel(Train, Train, sigma, l, type_dK_dl);   
     Eigen::MatrixXf K_inv = Kernel.inverse();
-    /*
+    
     Eigen::MatrixXf dsigma = 0.5*Multiply(labels.transpose(), K_inv.transpose());
     dsigma = Multiply(dsigma, dK_dsigma.transpose());
     dsigma = Multiply(dsigma, K_inv.transpose());
@@ -22,7 +22,7 @@ Eigen::MatrixXf GetGradient(Eigen::MatrixXf Train, Eigen::MatrixXf labels, float
     Eigen::MatrixXf trace_dsigma(1,1);
     trace_dsigma(0,0) = 0.5*Multiply(K_inv, dK_dsigma.transpose()).trace();
     dsigma = dsigma - trace_dsigma;
-    */
+    
     Eigen::MatrixXf dl = 0.5*Multiply(labels.transpose(), K_inv.transpose());
     dl = Multiply(dl, dK_dl.transpose());
     dl = Multiply(dl, K_inv.transpose());
@@ -30,9 +30,9 @@ Eigen::MatrixXf GetGradient(Eigen::MatrixXf Train, Eigen::MatrixXf labels, float
     Eigen::MatrixXf trace_dl(1,1);
     trace_dl(0,0) = 0.5*Multiply(K_inv, dK_dl.transpose()).trace();
     dl = dl - trace_dl;
-    /*
+    
     gradient(0,0) = dsigma(0,0); 
     gradient(0,1) = dl(0,0);
-    */
+    
     return dsigma;
 }
