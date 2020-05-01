311c5e4446b1ef6d7ea16e23d7d4bfb6e9f6d34f
Alex <luigi.taglialatela1996@gmail.com>
2020-05-01 20:02:46 +0200

    checking eigen 2

--- GetGradient.cu
+++ GetGradient.cu
@@ -13,7 +13,8 @@ Eigen::MatrixXf GetGradient(Eigen::MatrixXf Train, Eigen::MatrixXf labels, float
     Eigen::MatrixXf Kernel = GetKernel(Train, Train, sigma, l, type_standard);   
     Eigen::MatrixXf dK_dsigma = GetKernel(Train, Train, sigma, l, type_dK_dsigma);   
     Eigen::MatrixXf dK_dl = GetKernel(Train, Train, sigma, l, type_dK_dl);   
-    Eigen::MatrixXf dsigma = 0.5*Multiply(labels.transpose(), Kernel.inverse().transpose());
+    Eigen::MatrixXf dsigma = labels.transpose() * Kernel.inverse();
+    //Eigen::MatrixXf dsigma = 0.5*Multiply(labels.transpose(), Kernel.inverse().transpose());
     /*
     dsigma = Multiply(dsigma, dK_dsigma.transpose());
     dsigma = Multiply(dsigma, Kernel.inverse().transpose());
