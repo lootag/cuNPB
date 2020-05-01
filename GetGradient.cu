d8b487fa769c9625796a1cd18f8af68ef2caf594
Alex <luigi.taglialatela1996@gmail.com>
2020-05-01 19:55:30 +0200

    checking if eigen works

--- GetGradient.cu
+++ GetGradient.cu
@@ -13,7 +13,8 @@ Eigen::MatrixXf GetGradient(Eigen::MatrixXf Train, Eigen::MatrixXf labels, float
     Eigen::MatrixXf Kernel = GetKernel(Train, Train, sigma, l, type_standard);   
     Eigen::MatrixXf dK_dsigma = GetKernel(Train, Train, sigma, l, type_dK_dsigma);   
     Eigen::MatrixXf dK_dl = GetKernel(Train, Train, sigma, l, type_dK_dl);   
-    Eigen::MatrixXf dsigma = 0.5*Multiply(labels.transpose(), Kernel.inverse().transpose());
+    Eigen::MatrixXf dsigma = 0.5 * labels.transpose().dot(Kernel.inverse());
+    //Eigen::MatrixXf dsigma = 0.5*Multiply(labels.transpose(), Kernel.inverse().transpose());
     /*
     dsigma = Multiply(dsigma, dK_dsigma.transpose());
     dsigma = Multiply(dsigma, Kernel.inverse().transpose());
@@ -31,5 +32,5 @@ Eigen::MatrixXf GetGradient(Eigen::MatrixXf Train, Eigen::MatrixXf labels, float
     gradient(0,0) = dsigma(0,0);
     gradient(0,1) = dl(0,0);
     */
-    return labels.transpose();
+    return dsigma;
 }
