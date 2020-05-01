031cc279e451bce0b43c27f0e5abc651d4b327b7
Alex <luigi.taglialatela1996@gmail.com>
2020-05-02 01:06:54 +0200

    checking dl 5

--- GetGradient.cu
+++ GetGradient.cu
@@ -26,8 +26,8 @@ Eigen::MatrixXf GetGradient(Eigen::MatrixXf Train, Eigen::MatrixXf labels, float
     Eigen::MatrixXf dl = 0.5*Multiply(labels.transpose(), K_inv.transpose());
     dl = Multiply(dl, dK_dl.transpose());
     dl = Multiply(dl, K_inv.transpose());
-    /*
     dl = Multiply(dl, labels.transpose());
+    /*
     Eigen::MatrixXf trace_dl(1,1);
     trace_dl(0,0) = 0.5*Multiply(K_inv, dK_dl.transpose()).trace();
     dl = dl - trace_dl;
