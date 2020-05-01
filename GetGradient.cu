09264e5852c5554a23a0ad53a2e88c4226bf9c10
Alex <luigi.taglialatela1996@gmail.com>
2020-05-01 20:23:05 +0200

    printing out the matrices

--- GetGradient.cu
+++ GetGradient.cu
@@ -13,8 +13,23 @@ Eigen::MatrixXf GetGradient(Eigen::MatrixXf Train, Eigen::MatrixXf labels, float
     Eigen::MatrixXf Kernel = GetKernel(Train, Train, sigma, l, type_standard);   
     Eigen::MatrixXf dK_dsigma = GetKernel(Train, Train, sigma, l, type_dK_dsigma);   
     Eigen::MatrixXf dK_dl = GetKernel(Train, Train, sigma, l, type_dK_dl);   
-    //Eigen::MatrixXf dsigma = labels.transpose() * Kernel.inverse();
     Eigen::MatrixXf dsigma = 0.5*Multiply(labels.transpose(), Kernel.inverse().transpose());
+    std::cout << "Kernel" << std::endl;
+    for (int row = 0; row != Kernel.inverse().rows())
+    {
+        for(int col = 0; col != Kernel.inverse().cols())
+        {
+            std::cout << Kernel.inverse()(row, col) << std::endl;
+        }
+    }
+    std::cout << "labels.transpose()" << std::endl;
+    for(int row = 0; row != labels.transpose().rows(); row++)
+    {
+        for(int col = 0; col != labels.transpose().cols(); col++)
+        {
+            std::cout << labels.transpose()(row,col) << std::endl;
+        }
+    }
     /*
     dsigma = Multiply(dsigma, dK_dsigma.transpose());
     dsigma = Multiply(dsigma, Kernel.inverse().transpose());
@@ -32,5 +47,6 @@ Eigen::MatrixXf GetGradient(Eigen::MatrixXf Train, Eigen::MatrixXf labels, float
     gradient(0,0) = dsigma(0,0);
     gradient(0,1) = dl(0,0);
     */
+    std::cout << "dsigma" << std::enld; 
     return dsigma;
 }
