9eb0dbe7a0dee709a0f41cbc5f4f07f88c167383
Alex <luigi.taglialatela1996@gmail.com>
2020-05-02 10:44:15 +0200

    changing tolerance and adding some logs

--- Adam.cu
+++ Adam.cu
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <random>
 #include <math.h>
+#include <string>
 #include <eigen3/Eigen/Dense>
 #include "preprocessing.h"
 #include "optimizers.h"
@@ -11,7 +12,7 @@ std::vector<float> Adam(float alpha, float beta1, float beta2, Eigen::MatrixXf T
     int n_param = 2;
     std::default_random_engine generator;
     std::normal_distribution<float> distribution(1,1);
-    float tolerance = 0.0001;
+    float tolerance = 0.05;
     Eigen::MatrixXf m(n_param, 1);
     Eigen::MatrixXf v(n_param, 1);
     for(int row = 0; row != m.rows(); row++)
@@ -40,6 +41,7 @@ std::vector<float> Adam(float alpha, float beta1, float beta2, Eigen::MatrixXf T
     while(!(std::abs(gradient(0,0)) <= tolerance && std::abs(gradient(1,0)) <= tolerance) && !(iteration > maximum_iterations))
     {
         iteration += 1;
+        std::cout << "iteration number " + std::to_string(iteration) << std::endl;
         gradient = GetGradient(Train, labels, sigma, l);
         for(int row = 0; row != gradient_2.rows(); row++)
         {
@@ -48,10 +50,8 @@ std::vector<float> Adam(float alpha, float beta1, float beta2, Eigen::MatrixXf T
                 gradient_2(row, col) = gradient(row, col)*gradient(row, col);
             }
         }
-        std::cout << "dsigma" << std::endl;
-        std::cout <<  gradient(0,0) << std::endl;
-        std::cout << "dl" << std::endl;
-        std::cout << gradient(1,0) << std::endl;
+        std::cout << "dsigma " + std::to_string(gradient(0,0))<< std::endl;
+        std::cout << "dl " + std::to_string(gradient(1,0))  << std::endl;
         m = beta1 * m + (1 - beta1) * gradient;
         v = beta2 * v + (1 - beta2) * gradient_2;
         m_hat = m/(1 - pow(beta1, iteration));
