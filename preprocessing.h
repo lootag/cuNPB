c96b7074a2617747c03d37b317e83d560fd1f541
Alex <luigi.taglialatela1996@gmail.com>
2020-05-03 16:09:34 +0200

    fixing preprocessing.

--- preprocessing.h
+++ preprocessing.h
@@ -5,6 +5,7 @@
 #include "enumerators.h"
 
 Eigen::MatrixXf from_csv(std::string path, int rows, int cols);
+void to_csv(Eigen::MatrixXf Matrix, std::string file);
 Eigen::MatrixXf Multiply(Eigen::MatrixXf Left, Eigen::MatrixXf Right);
 Eigen::MatrixXf GetKernel(Eigen::MatrixXf A, Eigen::MatrixXf B, float sigma, float l, kernel_type type);
 Eigen::MatrixXf GetGradient(Eigen::MatrixXf Train, Eigen::MatrixXf labels, float sigma, float l);
